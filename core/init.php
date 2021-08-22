ee43ca0011b6f4034bdc1a8bc2b39fd63f274674
Elsayed93 <elsayedelbeshry119@gmail.com>
2021-08-22 22:47:01 +0200

    Initialization

---
+++ core/init.php
@@ -0,0 +1,27 @@
+<?php
+session_start();
+
+$GLOBALS['config'] = [
+    'mysql' => [
+        'host' => '127.0.0.1',
+        'username' => 'root',
+        'password' => '',
+        'db' => 'lr'
+    ],
+
+    'remember' => [
+        'cookie_name' => 'hash',
+        'cookie_expiry' => 6000000
+    ],
+
+    'session' => [
+        'session_name' => 'user'
+    ]
+];
+
+spl_autoload_register(function ($class) {
+    require_once 'classes/' . $class . '.php';
+});
+
+
+require_once 'functions/sanitize.php';
