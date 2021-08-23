a2f37a6571af3e7b3737d71454d6b8c2cc8c7d76
Elsayed93 <elsayedelbeshry119@gmail.com>
2021-08-23 16:45:40 +0200

    Config Class

---
+++ classes/Config.php
@@ -0,0 +1,24 @@
+<?php
+
+class Config
+{
+    public static function get($requiredConfig = null)
+    {
+        if ($requiredConfig) {
+            $config = $GLOBALS['config'];
+            $data = explode('/', $requiredConfig);
+
+            foreach ($data  as  $value) {
+                if (isset($config[$value])) {
+                    $config = $config[$value];
+                } else {
+                    return false;
+                }
+            }
+
+            return $config;
+        } else {
+            return '<h3>get require a parameter';
+        }
+    }
+}
